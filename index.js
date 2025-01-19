4cd47f23a3955e85e912ad7d073d04a607e4c2fd
vinceEmeka <chimezievinz@gmail.com>
2025-01-19 23:39:53 +0100

    class update 2

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
-
-
 class User {
+  constructor(userId, password) {
+    this.userId = userId;
+    this.password = password;
+  }
 
-    constructor(userId, password) {
-        this.userId = userId;
-        this.password = password;
+  //static method to login
+  static verifyLogin() {
+    if (User.userId && User.password === User) {
+      console.log("Login Success!!");
     }
-   
-    //static method to login 
-    static verifyLogin(){
-        if(User.userId && User.password === User) {
-            console.log("Login Success!!");
-            
-        }
-    }
+  }
 }
 
-// Customer 
+// Customer
 
 class Customer {
-    constructor(){
-        
+    constructor(customerName, customerEmail, customerNumber) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerNumber = customerNumber;
+      }
+}
+
+class activeSubsciption {
+  constructor(customerName, customerEmail, customerNumber) {
+    this.customerName = customerName;
+    this.customerEmail = customerEmail;
+    this.customerNumber = customerNumber;
+  }
+
+  static(planName, planCost) {
+    this.planName = planName;
+    this.planCost = planCost;
+  }
+
+  subcribe() {
+    console.log("Pay here!!");
+  }
+
+  renewSubscription() {
+    subcribe(activeSubsciption.planName);
+  }
+
+  static expirationReminder() {
+    console.log("You have 7 days before your plan expires");
+  }
+}
+
+class subcriptionPlans {
+  static(planName, planDuration, planCost) {
+    this.planName = planName;
+    this.planDuration = planDuration;
+    this.planCost = planCost;
+  }
+
+  static subcribe() {
+    console.log("Pay here!!");
+  }
+}
+
+class Administrator {
+  constructor(adminName, email) {
+    this.adminName = adminName;
+    this.email = email;
+  }
+
+  static updateSubscriptionPlans() {}
+  static updateActiveSubscription() {
+    if (Customer.subcribe()) {
+      console.log("new subscription alert");
     }
+  }
 }
