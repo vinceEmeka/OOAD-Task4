8046900a56c05154ac1a7ad0608be3772edc0120
vinceEmeka <chimezievinz@gmail.com>
2025-01-19 23:21:41 +0100

    class update

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+class User {
+
+    constructor(userId, password) {
+        this.userId = userId;
+        this.password = password;
+    }
+   
+    //static method to login 
+    static verifyLogin(){
+        if(User.userId && User.password === User) {
+            console.log("Login Success!!");
+            
+        }
+    }
+}
+
+// Customer 
+
+class Customer {
+    constructor(){
+        
+    }
+}
